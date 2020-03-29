5f070d8af74f28631824358b9bc380f34e37ce0c
amitswaraj61 <aswaraj78@gmail.com>
2020-03-29 22:07:00 +0530

    total salary 100 days

--- welcome.sh
+++ welcome.sh
@@ -90,7 +90,7 @@ do
 
         $empPartTime )
               empHr=4 ;;
-         
+
          * )
               empHr=0 ;;
          esac
@@ -98,8 +98,48 @@ do
          salary=$(( $empWagePerHr * $empHr ))
          echo "daily salary := $salary"
          TotalSalary=$(( $salary + $totalSalary )) 
+
+         TotalSalary=$(( $salary + $totalSalary ))
  done
 }
 
 EmployeeMang
 echo "total salary is = $TotalSalary"
+
+#uc6
+
+empWagePerHr=20 
+empFullTime=1
+empPartTime=2 
+dayzPerMonth=20
+TotalSalary=0 
+Total_hr=0
+Total_Max_hr=100 
+
+function EmployeeManagement () {
+
+	while [ $Total_hr -le $Total_Max_hr  ]
+	do
+	random=$((RANDOM%3))
+	case $random in
+
+     	$empFullTime )
+		empHr=8 ;;
+
+	$empPartTime )
+		empHr=4 ;;
+	
+	* )
+		empHr=0 ;;
+	esac
+		
+	Total_hr=$(($Total_hr + $empHr))
+
+	salary=$(( $empWagePerHr * $Total_hr ))
+
+	TotalSalary=$(( $salary + $TotalSalary ))
+	done
+}
+
+EmployeeManagement
+ echo "Total Salary for 100hr is " = $TotalSalary 
