bf30d5a7d779cd7990bce47ec5651467325b47c8
amitswaraj61 <aswaraj78@gmail.com>
2020-03-29 22:12:30 +0530

    function total work

--- welcome.sh
+++ welcome.sh
@@ -143,3 +143,42 @@ function EmployeeManagement () {
 
 EmployeeManagement
  echo "Total Salary for 100hr is " = $TotalSalary 
+
+#uc7
+
+EMP_WAGE_PER_HR=20 
+EMP_FULL_TIME=1
+EMP_PART_TIME=2 
+DAYZ_PER_MONTH=20
+TOTAL_MAX_HR=100
+
+totalSalary=0 
+totalHr=0
+ 
+
+function GetWorkingHours ()
+{
+	
+	for (( i=1; i<=$DAYZ_PER_MONTH; i++ ))
+	do
+	random=$((RANDOM%3))
+	case $random in
+
+     	$EMP_FULL_TIME )
+		EMP_HR=8 ;;
+
+	$EMP_PART_TIME )
+		EMP_HR=4 ;;
+	
+	* )
+		EMP_HR=0 ;;
+	esac
+		
+	totalHr=$(($totalHr + $EMP_HR))
+done
+	
+}
+
+GetWorkingHours 
+
+ echo "Total work hours is " = $totalHr  
