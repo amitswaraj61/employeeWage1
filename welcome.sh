6055bbd55d0fffc8a2b372def03bdd62c22bd851
amitswaraj61 <aswaraj78@gmail.com>
2020-03-29 20:59:50 +0530

    employeeAttendence

--- welcome.sh
+++ welcome.sh
@@ -3,3 +3,21 @@
 #WelcomeMessage
 
 echo "welcome to employeeWageComputation"
+
+#employeeAttendence
+
+echo "enter the num"
+read num
+present=0
+absent=0
+for(( i=1 ; i <=num; i++ ))
+  do
+     if [ $(( $RANDOM % 2)) -eq 0 ]
+        then
+          (( present++ ))
+    else
+          (( absent++ ))
+    fi
+ done
+echo "Employee is presnt : $present"
+echo "Employee is absent : $absent"
