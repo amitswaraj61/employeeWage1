ce471e55e0a3e13cdd00ed21e4f0e6b34e1292b0
amitswaraj61 <aswaraj78@gmail.com>
2020-03-29 22:20:56 +0530

    array using

--- welcome.sh
+++ welcome.sh
@@ -182,3 +182,49 @@ done
 GetWorkingHours 
 
  echo "Total work hours is " = $totalHr  
+
+#uc8
+
+EMP_WAGE_PER_HR=20 
+EMP_FULL_TIME=1
+EMP_PART_TIME=2 
+DAYZ_PER_MONTH=20
+TOTAL_MAX_HR=100
+
+totalSalary=0 
+totalHr=0
+salary=0
+
+declare -a ArrayForWages
+
+function GetWorkingHours ()
+{
+
+	for (( i=1; i<=$DAYZ_PER_MONTH; i++ ))
+	do
+	random=$((RANDOM%3))
+	case $random in
+
+	$EMP_FULL_TIME )
+		EMP_HR=8 ;;
+
+	$EMP_PART_TIME )
+		EMP_HR=4 ;;
+
+	* )
+		EMP_HR=0 ;;
+	esac
+
+	totalHr=$(( $totalHr + $EMP_HR ))
+	Salary=$(( $EMP_WAGE_PER_HR * $EMP_HR ))
+
+	totalSalary=$(( $EMP_WAGE_PER_HR * $totalHr  ))
+
+	ArrayForWages=( $Salary $totalSalary )
+        echo "Daily wages along with total wages" ${ArrayForWages[@]} 
+
+
+        done
+}
+
+	GetWorkingHours 
