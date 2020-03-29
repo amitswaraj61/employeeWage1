94fcb33cf51fcf1975ea8e52b38ac9d90f6aac82
amitswaraj61 <aswaraj78@gmail.com>
2020-03-29 22:26:18 +0530

    storeday

--- welcome.sh
+++ welcome.sh
@@ -228,3 +228,59 @@ function GetWorkingHours ()
 }
 
 	GetWorkingHours 
+
+#uc9
+
+EMP_WAGE_PER_HR=20 
+EMP_FULL_TIME=1
+EMP_PART_TIME=2 
+DAYZ_PER_MONTH=20
+TOTAL_MAX_HR=100
+
+totalSalary=0 
+totalHr=0
+salary=0
+
+
+declare -A wages 
+
+function GetWorkingHours ()
+{
+	
+	for (( i=1; i<=$DAYZ_PER_MONTH; i++ ))
+	do
+       echo "Total work hours is = " $totalHr
+
+       echo "Total wages for month is =" $totalSalary
+
+	random=$((RANDOM%3))
+	case $random in
+
+     	$EMP_FULL_TIME )
+		EMP_HR=8 ;;
+
+	$EMP_PART_TIME )
+		EMP_HR=4 ;;
+	
+	* )
+		EMP_HR=0 ;;
+	esac
+		
+	totalHr=$(( $totalHr + $EMP_HR ))
+	salary=$(( $EMP_WAGE_PER_HR * $EMP_HR ))
+
+
+	totalSalary=$(( $EMP_WAGE_PER_HR * $totalHr  ))
+	wages[i]="$salary"
+
+	echo "wages per day =" ${wages[i]}
+
+	wages[totalSalary]="$totalSalary"
+
+	echo "totalSalary =" ${wages[totalSalary]}
+done
+	
+}
+
+GetWorkingHours 
+
